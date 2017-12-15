1b54696ca76349b3b10dae7aeff808d72995eb31
Charles Pascoe <charles@cpascoe.co.uk>
2017-12-15 16:26:12 +0000

    feat(lib/index): Create MultiOptionalArgument parser

--- lib/index.ts
+++ lib/index.ts
@@ -215,6 +215,57 @@ export class OptionalArgument<T> extends Optional<T> {
 }
 
 
+export interface IMultiOptionalArgumentOptions<T> extends IOptionalOptions {
+  parse: (val: string) => T;
+}
+
+
+export class MultiOptionalArgument<T> extends Optional<T[]> {
+  private parse: (val: string) => T;
+
+  constructor(options: IMultiOptionalArgumentOptions<T>) {
+    super(options);
+    this.parse = options.parse;
+  }
+
+  public evaluate(tokens: Token[]): IEvaluatedArgument<T[]> {
+    let newTokens = tokens.map(token => token);
+
+    const result: T[] = [];
+
+    for (let i = newTokens.length - 1; i >= 0; i--) {
+      const token = newTokens[i];
+
+      if (token.type === 'short' && token.value === this.short || token.type === 'long' && token.value === this.long) {
+        if (token.argument !== null) {
+          result.unshift(this.parse(token.argument));
+          newTokens.splice(i, 1);
+          continue;
+        }
+
+        if (i + 1 >= newTokens.length) {
+          throw new Error(`${this.getShortLongOptions()} requires an argument`);
+        }
+
+        const nextToken = newTokens[i + 1];
+
+        if (nextToken.type !== 'arg') {
+          throw new Error(`${this.getShortLongOptions()} requires an argument`);
+        }
+
+        result.unshift(this.parse(nextToken.argument));
+        newTokens.splice(i, 2);
+      }
+    }
+
+    return {
+      newTokens,
+      value: result
+    };
+  }
+}
+
+
 export interface IPositionalArgumentOptions<T> {
   description?: string;
   default: T;

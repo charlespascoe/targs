b270ae7d2a0c6463b1823dee051e3d3c679bacb7
Charles Pascoe <charles@cpascoe.co.uk>
2017-12-15 19:33:36 +0000

    feat(lib/index:MultiPositionalArgument): Add minimum number of arguments

--- lib/index.ts
+++ lib/index.ts
@@ -319,11 +319,17 @@ export class PositionalArgument<T> extends Argument<T> {
 export interface IMultiPositionalArgumentOptions<T> {
   description?: string;
   parse: (val: string) => T;
+  metaVar: string;
+  min?: number;
 }
 
 
 export class MultiPositionalArgument<T> extends Argument<T[]> {
   public readonly description: string | null;
+
+  public readonly metaVar: string;
+
+  public readonly min: number = 0;
 
   private readonly parse: (val: string) => T;
 
@@ -331,13 +337,24 @@ export class MultiPositionalArgument<T> extends Argument<T[]> {
     super();
 
     this.description = options.description || null;
+    this.metaVar = options.metaVar;
     this.parse = options.parse;
+
+    if (typeof options.min === 'number') {
+      this.min = options.min;
+    }
   }
 
   public evaluate(tokens: Token[]): IEvaluatedArgument<T[]> {
+    const parsedArgs = tokens.filter(token => token.type === 'arg').map(token => this.parse((<IArgumentToken>token).argument));
+
+    if (parsedArgs.length < this.min) {
+      throw new Error(`${this.metaVar} requires at least ${this.min} argument${this.min === 1 ? '': 's'}`);
+    }
+
     return {
       newTokens: tokens.filter(token => token.type !== 'arg'),
-      value: tokens.filter(token => token.type === 'arg').map(token => this.parse((<IArgumentToken>token).argument))
+      value: parsedArgs
     };
   }
 }
